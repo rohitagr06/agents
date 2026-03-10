4a882c96797ec7fa0f0393851c8a32d3a6eb2123
rohitagr06 <rohitagr06@gmail.com>
2026-03-10 11:54:39 +0530

    added my app.py file

--- 1_foundations/copy_app.py
+++ 1_foundations/copy_app.py
@@ -1,14 +1,13 @@
-from urllib import response
 from dotenv import load_dotenv
-import json
 from openai import OpenAI
+import json
 import os
 import requests
 from pypdf import PdfReader
 import gradio as gr
 
+
 load_dotenv(override=True)
-google_api_key = os.getenv('GOOGLE_API_KEY')
 
 def push(text):
     requests.post(
@@ -20,17 +19,18 @@ def push(text):
         }
     )
 
+
 def record_user_details(email, name="Name not provided", notes="not provided"):
     push(f"Recording {name} with email {email} and notes {notes}")
-    return {"Recorded" : "OK"}
+    return {"recorded": "ok"}
 
 def record_unknown_question(question):
     push(f"Recording {question}")
-    return {"Recorded" : "OK"}
+    return {"recorded": "ok"}
 
 record_user_details_json = {
     "name": "record_user_details",
-    "description": "Use this tool to record that a user is interested in touch and provide and email address",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
     "parameters": {
         "type": "object",
         "properties": {
@@ -40,11 +40,12 @@ record_user_details_json = {
             },
             "name": {
                 "type": "string",
-                "description": "The user's name, if they provided it."
-            },
+                "description": "The user's name, if they provided it"
+            }
+            ,
             "notes": {
                 "type": "string",
-                "description": "The additional information about the conversation that's worth recording to give context."
+                "description": "Any additional information about the conversation that's worth recording to give context"
             }
         },
         "required": ["email"],
@@ -54,7 +55,7 @@ record_user_details_json = {
 
 record_unknown_question_json = {
     "name": "record_unknown_question",
-    "description": "Use this tool to record any question that couldn't be answered and you didn't know the answer",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
     "parameters": {
         "type": "object",
         "properties": {
@@ -68,62 +69,66 @@ record_unknown_question_json = {
     }
 }
 
-tools = [{"type": "function", "function": record_user_details_json}, 
-{"type": "function", "function": record_unknown_question_json}]
+tools = [{"type": "function", "function": record_user_details_json},
+        {"type": "function", "function": record_unknown_question_json}]
+
 
 class Me:
+
     def __init__(self):
-        self.gemini = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/") 
-        self.model_name = "gemini-2.5-flash"
-        self.name = "Rohit Agrawal"
+        self.openai = OpenAI()
+        self.name = "Ed Donner"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
         for page in reader.pages:
             text = page.extract_text()
             if text:
                 self.linkedin += text
-        with open("me/summary.txt", "r", encoding="UTF-8") as f:
+        with open("me/summary.txt", "r", encoding="utf-8") as f:
             self.summary = f.read()
+
 
     def handle_tool_call(self, tool_calls):
         results = []
         for tool_call in tool_calls:
             tool_name = tool_call.function.name
             arguments = json.loads(tool_call.function.arguments)
-            print(f"Tool Called: {tool_name}", flush=True)
+            print(f"Tool called: {tool_name}", flush=True)
             tool = globals().get(tool_name)
             result = tool(**arguments) if tool else {}
-            results.append({"role": "tool", "content": json.dumps(result), "tool_call_id": tool_call.id})
+            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
         return results
-
+    
     def system_prompt(self):
-        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website. \
-Particularly questions related to {self.name}'s career, background, skills and experience. \
+        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
+particularly questions related to {self.name}'s career, background, skills and experience. \
 Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
-You are given a summary of {self.name}'s background and linkedin profile which you can use to give answers. \
-Be professional and engaging, as if talking to a potential client or employer who came accross the website. \
-If you don't know the answer of any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
-If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using record_user_details tool."
-        system_prompt += f"\n\n## Summary: \n{self.summary}\n\n## Linkedin Profile: \n{self.linkedin}\n\n"
+You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
+Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+
+        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
         system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
         return system_prompt
-
+    
     def chat(self, message, history):
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
-
         while not done:
-            response = self.gemini.chat.completions.create(model=self.model_name, messages=messages, tools = tools)
-            if response.choices[0].finish_reason == "tool_calls":
+            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
                 results = self.handle_tool_call(tool_calls)
                 messages.append(message)
-                messages.extent(results)
+                messages.extend(results)
             else:
                 done = True
         return response.choices[0].message.content
+    
 
 if __name__ == "__main__":
     me = Me()
-    gr.ChatInterface(me.chat, type = "messages").launch()
+    gr.ChatInterface(me.chat, type="messages").launch()
+    
