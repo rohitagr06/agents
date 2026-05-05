74fdb64f4d3eed2c52f12c8d41bf5f92c35e6cc9
rohitagr06 <rohitagr06@gmail.com>
2026-05-05 18:14:47 +0530

    updated deep research

--- 2_openai/deep_research_rohit/app.py
+++ 2_openai/deep_research_rohit/app.py
@@ -1,24 +1,16 @@
 import gradio as gr
 from research_manager import ResearchManager
+import asyncio
 
-async def async_run(query: str):
-
-    output = ""
+async def run(query: str):
     async for chunk in ResearchManager().run(query):
-        output += chunk
-        yield output
-
-def run(query: str):
-    if not query.strip():
-        return "Please enter a valid research query."
-    return async_run(query)
+        yield chunk
 
 with gr.Blocks(theme=gr.themes.Default(primary_hue="sky")) as ui:
     gr.Markdown("# Deep Research")
     query_textbox = gr.Textbox(label="What topic would you like to research?")
     run_button = gr.Button("Run", variant="primary")
     report = gr.Markdown(label="Report")
-
     run_button.click(fn=run, inputs=query_textbox, outputs=report)
     query_textbox.submit(fn=run, inputs=query_textbox, outputs=report)
 
